676d23e0f2f7bc032479726536bc003386fd3bad
YAMADA Tsuyoshi <tyamada@minimum2scp.org>
2015-07-13 13:06:42 +0900

    add `drive_env sheet to_env` subcommand

--- lib/drive_env/cli/sheet.rb
+++ lib/drive_env/cli/sheet.rb
@@ -25,6 +25,29 @@ module DriveEnv
         puts table.to_s
       end
 
+      desc "to_env SHEET_URI", ""
+      def to_env(uri)
+        spreadsheet = session.spreadsheet_by_url(uri)
+        if uri =~ /#gid=(\d+)/
+          ws = spreadsheet.worksheet_by_gid($1)
+        else
+          ws = spreadsheet.worksheets[0]
+        end
+        ws.rows.each.with_index do |row, idx|
+          if row[0] =~ /\A\s*#/
+            puts row.join(' ')
+          elsif row.size > 2
+            puts "#{row[0]}=#{row[1]} # #{row[2..-1].join(' ')}"
+          elsif row.size == 2
+            puts "#{row[0]}=#{row[1]}"
+          elsif row.size == 1
+            puts "# #{row[0]}"
+          else
+            # ignore
+          end
+        end
+      end
+
       no_commands do
         def session
           unless @session
