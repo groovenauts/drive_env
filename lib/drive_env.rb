80377b9d741488d83b3d3626431140c536c766de
Tomohiko OKAZAKI <t-okazaki@groovenauts.jp>
2021-09-07 17:20:58 +0900

    support serivce account authentication

--- lib/drive_env.rb
+++ lib/drive_env.rb
@@ -9,13 +9,21 @@ module DriveEnv
   class << self
     def authorizer(client_id, client_secret, token_store_file)
       unless @authorizer
-        client_id = Google::Auth::ClientId.new(client_id, client_secret)
+        cred = ENV['GOOGLE_APPLICATION_CREDENTIALS']
         scope = %w[
           https://www.googleapis.com/auth/drive
           https://spreadsheets.google.com/feeds/
         ]
-        token_store = Google::Auth::Stores::FileTokenStore.new(file: token_store_file)
-        @authorizer = Google::Auth::UserAuthorizer.new(client_id, scope, token_store)
+        if cred.nil?
+          client_id = Google::Auth::ClientId.new(client_id, client_secret)
+          token_store = Google::Auth::Stores::FileTokenStore.new(file: token_store_file)
+          @authorizer = Google::Auth::UserAuthorizer.new(client_id, scope, token_store)
+        else
+          @authorizer = Google::Auth::ServiceAccountCredentials.make_creds(
+            json_key_io: File.open(cred),
+            scope: scope
+          )
+        end
       end
       @authorizer
     end
